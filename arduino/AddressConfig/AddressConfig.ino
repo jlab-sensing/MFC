3400afae6f4212a5d36e94900f377c041d0f253d
John Madden <jmadden173@protonmail.com>
2022-06-23 14:01:30 -0700

    Added boilerplate code for SDI

--- arduino/AddressConfig/AddressConfig.ino
+++ arduino/AddressConfig/AddressConfig.ino
@@ -1,12 +1,20 @@
-int i = 0;
+#include "SDISerial.h"
 
-void setup() {
+/** Max Delay between the measure and data commands */
+#define SENSOR_DELAY 1000
+
+/** Pin used for line. Must be interrupt pin. */
+#define DATA_PIN 13
+
+SDISerial sdi_serial_connection(DATA_PIN);
+
+void setup()
+{
 	Serial.begin(9600);
+	Serial.println("TEROS-12 Address configurator, compiled on " __DATE__ " " __TIME__);
 
-	Serial.println("Hello World");
+	sdi_serial_connection.begin();
+	delay(3000);
 }
 
-void loop() {
-	Serial.println(i++);
-	delay(500);
-}
+void loop() {}
