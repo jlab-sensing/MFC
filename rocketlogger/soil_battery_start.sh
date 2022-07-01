69a35874e3618c591cdff6912fe463c4f4fcba7e
John Madden <jmadden173@protonmail.com>
2022-07-01 13:17:56 -0700

    Updated start script

--- rocketlogger/soil_battery_start.sh
+++ rocketlogger/soil_battery_start.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
-NUMBER=`cat /home/rocketlogger/num.txt`
-FNAME=soil_${DATE_WITH_TIME}_${NUMBER}.csv
+TIMESTAMP=`date "+%Y%m%d-%H%M%S"`
+LOGDIR=/home/rocketlogger/soil_battery
 DEV=/dev/ttyACM0
-
-echo $FNAME
 
 # Stop logging
 rocketlogger stop
 sleep 1s
 # Start logging
-rocketlogger cont -r 1 -f /home/rocketlogger/soil_battery/$FNAME -format csv -ch 0,1,2,3,4,5 -size 1g -d 0 -w 0
-echo $(( $NUMBER+1 )) > /home/rocketlogger/num.txt
+rocketlogger cont -r 1 -f ${LOGDIR}/soil_${TIMESTAMP}.csv -format csv -ch 0,1,2,3,4,5 -size 1g -d 0 -w 0
 
 # Log teros sensor if connected
 if test -e "$DEV"; then
-	teroslogger -q -o /home/rocketlogger/soil_battery/tmp_$DATE_WITH_TIME $DEV 
+	teroslogger -q -o ${LOGDIR}/teros_${TIMESTAMP}.csv $DEV 
 fi
