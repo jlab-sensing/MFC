73e7c36dc2b2537bced72758059dd8996066fdec
John Madden <jmadden173@protonmail.com>
2022-07-01 13:13:22 -0700

    Updated teroslogger args

--- rocketlogger/soil_battery_start.sh
+++ rocketlogger/soil_battery_start.sh
@@ -7,11 +7,14 @@ DEV=/dev/ttyACM0
 
 echo $FNAME
 
+# Stop logging
 rocketlogger stop
 sleep 1s
+# Start logging
 rocketlogger cont -r 1 -f /home/rocketlogger/soil_battery/$FNAME -format csv -ch 0,1,2,3,4,5 -size 1g -d 0 -w 0
 echo $(( $NUMBER+1 )) > /home/rocketlogger/num.txt
 
+# Log teros sensor if connected
 if test -e "$DEV"; then
-	teroslogger -t $DEV -n $NUMBER
+	teroslogger -q -o /home/rocketlogger/soil_battery/tmp_$DATE_WITH_TIME $DEV 
 fi
