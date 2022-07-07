5aaa46a525815163ccd40cca38ceaece316ff491
John Madden <jmadden173@protonmail.com>
2022-07-07 08:46:40 -0700

    Added params

--- data_processing/pull_data.sh
+++ data_processing/pull_data.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+USER="rocketlogger"
 HOSTS=("rocket1-jlab.ucsc.edu" "rocket3-jlab.ucsc.edu")
+REMOTE_PATH="/home/rocketlogger/soil_battery/soil_latest.csv"
+LOCAL_PATH=`pwd`
+
+SCP_OPTIONS="-P 2322 -o 'PubkeyAcceptedKeyTypes +ssh-rsa'"
 
 for host in "${HOSTS[@]}"; do
+	scp $SCP_OPTIONS ${host}:${REMOTE_PATH} ${
 	echo $host
+	echo $REMOTE_PATH
+	echo $LOCAL_PATH
+	echo $SCP_OPTIONS
 done
